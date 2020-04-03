4b62ce998e10cb5e6a996f43c7585bef90a32371
Kuhwan An <usimebul@signgate.com>
2020-04-03 11:31:58 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,131 @@
+"use strict"; // Start of use strict
+
+(function($) {
+    var elements = $("[include-html]");
+
+    elements.each(function(index, element) {
+        (function() {
+            var src = element.getAttribute("include-html");
+            $.get(src, function(data, status) {
+                if (status === "success") element.innerHTML = data;
+                else element.innerHTML = "Page not found";
+                element.removeAttribute('include-html');
+            })
+        })();
+    });
+})(jQuery);
+
+
+window.onload = function() {
+    // Toggle the side navigation
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+            $('.sidebar .collapse').collapse('hide');
+        };
+    });
+
+    // Close any open menu accordions when window is resized below 768px
+    $(window).resize(function() {
+        if ($(window).width() < 768) {
+            $('.sidebar .collapse').collapse('hide');
+        };
+    });
+
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+        if ($(window).width() > 768) {
+            var e0 = e.originalEvent,
+                delta = e0.wheelDelta || -e0.detail;
+            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+            e.preventDefault();
+        }
+    });
+
+    // Scroll to top button appear
+    $(document).on('scroll', function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Smooth scrolling using jQuery easing
+    $(document).on('click', 'a.scroll-to-top', function(e) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+    });
+
+
+    // Active current sidebar menu.
+    var path = window.location.pathname;
+    $(".collapse-item").each(function(index, element) {
+        var collapseItem = $(element);
+        if (collapseItem.attr('href') === path) {
+            collapseItem.addClass('active');
+            collapseItem.parent().parent().addClass('show');
+            collapseItem.parent().parent().parent().addClass('active');
+            collapseItem.parent().parent().parent().children('a').removeClass('collapsed');
+        }
+    });
+}
+
+// SPA using hash
+
+var root = $('#app-root');
+
+function router(path) {
+    var hash = location.hash.replace('#', '');
+    if (!hash) hash = "home";
+    $.get(hash + ".html", function(data, status) {
+        if (status === "success")
+            root.html(data);
+        else {
+            $.get("/404.html", function(data, status) {
+                root.html(data);
+            })
+        }
+    }).fail(function() {
+        $.get("/404.html", function(data, status) {
+            root.html(data);
+        })
+    }).always(function() {
+        // Add breadcrumb
+        var urls = window.location.href.split('/');
+        var breadcrumbWrapper = $("#breadcrumb-wrapper");
+
+        breadcrumbWrapper.html("");
+
+        var url = ""
+        for (var i = 4; i < urls.length; i++) {
+            if (["secukit-nxs", "secukit-html5"].includes(urls[i]))
+                breadcrumbWrapper.append("<li class='breadcrumb-item'><span>Client Toolkits</span></li>");
+
+            url = urls[i].replace('#', '');
+            if (url === "" || url === this.undefined)
+                break;
+
+            breadcrumbWrapper.append("<li class='breadcrumb-item'><span>" + url + "</span></li>");
+        }
+
+        $("#top-navbar").children().each(function(index, element) {
+            var navItem = $(element);
+            navItem.removeClass('active');
+
+            var href = navItem.children('a').attr('href').replace('#', '');
+
+            if (href === hash || (href === "" && hash === "home")) {
+                navItem.addClass('active');
+            }
+        });;
+    });
+}
+
+window.onhashchange = router;
+window.addEventListener('DOMContentLoaded', router);
---
+++ js/polyfill.js
@@ -0,0 +1,38 @@
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
+
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, "includes", {
+        enumerable: false,
+        value: function(obj) {
+            var newArr = this.filter(function(el) {
+                return el == obj;
+            });
+            return newArr.length > 0;
+        }
+    });
+}
+
+
+if (!Object.entries)
+    Object.entries = function(obj) {
+        var ownProps = Object.keys(obj),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+        while (i--)
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+        return resArray;
+    };
